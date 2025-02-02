8e509c0b8ab3a97a4a4963e88cb35faa16b9596c
Nikhil <codewithniku@gmail.com>
2025-02-02 21:48:29 +0530

    Add controller and Bcrypt

---
+++ controller/user.controller.js
@@ -0,0 +1,93 @@
+const userModel = require('../models/user.models');
+const {generateToken} = require('../auth');
+const bcrypt = require('bcrypt');
+
+
+// Register Controller
+async function handelRegister(req,res){
+
+    try{
+        const {username,email,password} = req.body;
+
+        const saltRounds = 10;
+        const hashPassword = await bcrypt.hash(password, saltRounds);
+
+        const newUser = new userModel({username,email,password: hashPassword});
+        const userSave = await newUser.save();
+        
+        if(!userSave){return res.status(401).json({message : "Server unable to Create User"})};
+        console.log("User Registerd");
+
+        // Create Payload for JWT ( use any field [ id , username , e.t.c.. ] ) 
+        const payload = { username: userSave.username }
+
+        // Generate token at REGISTER
+        // Pass payload for Generate Token
+        const token = generateToken(payload);
+        console.log("Register Token : ",token);
+
+        return res.status(201).json({ message: `${userSave.username} is Created` });
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message : "Internal Server Error"})
+    }   
+
+}
+
+// Login Controller
+async function handelLogin(req,res){
+    try{
+        const {username,password} = req.body;
+        const verifyUser = await userModel.findOne({username});
+
+        if (!verifyUser) {
+            return res.status(401).json({ error: 'Invalid username or password' });
+        }
+
+        const isMatch = await bcrypt.compare(password,verifyUser.password);
+
+        if (!isMatch) {
+            return res.status(401).json({ error: 'Invalid username or password' });
+        }  
+
+        // Generate token at LOGIN
+        const payload = {
+            username : verifyUser.username
+        }
+
+        // Pass Payload
+        const token = generateToken(payload);
+        console.log("Login Token : ",token);
+        
+        console.log("User Login");
+        return res.status(201).json({ message: `${verifyUser.username} is Login` });
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message : "Internal Server Error"})
+    }   
+}
+
+//Profile Controller
+async function handelProfileView(req,res){
+    try{
+        const profileData = req.verifyToken;
+        console.log(profileData);
+
+        const profileId = profileData.username;
+        const userprofile =  await userModel.findOne({username:profileId});
+        
+        return res.status(200).json(userprofile);
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message : "Internal Server Error"})
+    }
+}
+
+module.exports = {
+    handelRegister,
+    handelLogin,
+    handelProfileView
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,108 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const userModel = require('../models/user.models');
-const {jwtauthMiddleware,generateToken} = require('../auth')
+const {jwtauthMiddleware} = require('../auth');
+const {handelRegister,handelLogin,handelProfileView} = require('../controller/user.controller')
 
 
 // Register Route
-
-router.post('/register', async(req,res)=>{
-
-    try{
-        const {username,email,password} = req.body;
-        const newUser = new userModel({username,email,password});
-        const userSave = await newUser.save();
-        
-        if(!userSave){return res.status(401).json({message : "Server unable to Create User"})};
-        console.log("User Registerd");
-
-        // Create Payload for JWT ( use any field [ id , username , e.t.c.. ] ) 
-        const payload = { username: userSave.username }
-
-        // Generate token at REGISTER
-        // Pass payload for Generate Token
-        const token = generateToken(payload);
-        console.log("REgister Token : ",token);
-
-        res.status(201).json({ message: `${userSave.username} is Created` });
-
-    }catch(error){
-        console.log(error);
-        res.status(500).json({message : "Internal Server Error"})
-    }   
-
-})
-
+router.post('/register',handelRegister)
 
 // Login Route
-
-router.post('/login',async(req,res)=>{
-
-    try{
-        const {username,password} = req.body;
-        const verifyUser = await userModel.findOne({username});
-
-        if (!verifyUser) {
-            return res.status(401).json({ error: 'Invalid username or password' });
-        }
-
-        if (password != verifyUser.password) {
-            return res.status(401).json({ error: 'Invalid username or password' });
-        }       
-
-        // Generate token at LOGIN
-        const payload = {
-            username : verifyUser.username
-        }
-
-        // Pass Payload
-        const token = generateToken(payload);
-        console.log("Login Token : ",token);
-        
-        console.log("User Login");
-        res.status(201).json({ message: `${verifyUser.username} is Login` });
-
-    }catch(error){
-        console.log(error);
-        res.status(500).json({message : "Internal Server Error"})
-    }   
-
-})
-
+router.post('/login',handelLogin);
 
 // See Profile after Login
+router.get('/profile',jwtauthMiddleware,handelProfileView);
 
-router.get('/profile',jwtauthMiddleware, async (req,res)=>{
-
-    try{
-
-        const profileData = req.verifyToken;
-        console.log(profileData);
-
-        const profileId = profileData.username;
-        const userprofile =  await userModel.findOne({username:profileId});
-        
-        res.status(200).json(userprofile);
-
-    }catch(error){
-        console.log(error);
-        res.status(500).json({message : "Internal Server Error"})
-    }
-
-})
-
-
-// GET method to get the person
-
-router.get('/', jwtauthMiddleware, async (req, res) =>{
-    try{
-        const data = await userModel.find();
-        console.log('All User Get');
-        res.status(200).json(data);
-    }catch(err){
-        console.log(err);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-})
 
 module.exports = router;
